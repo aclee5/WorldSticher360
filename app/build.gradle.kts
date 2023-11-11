2597ba7043bb1d56ef3bd8887307b78e6ba5cf74
Alexis Lee <aclee2001@gmail.com>
2023-11-10 17:46:12 -0800

    Seeing if Jar file implementation works

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(files("D:\\SFU COURSES\\FALL 2023\\IAT 359\\Work\\Final Project\\app\\libs\\javacpp.jar"))
+    implementation(files("D:\\SFU COURSES\\FALL 2023\\IAT 359\\Work\\Final Project\\app\\libs\\javacv.jar"))
+    implementation(files("D:\\SFU COURSES\\FALL 2023\\IAT 359\\Work\\Final Project\\app\\libs\\opencv.jar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("org.bytedeco:javacv-platform:1.5.7")
+//    implementation ("org.bytedeco:javacv-platform:1.5.7")
 }
