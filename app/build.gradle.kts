34d247340faf768537869b7163d40ffd807b04fd
Alexis Lee <aclee2001@gmail.com>
2023-11-12 15:24:47 -0800

    implementing cameraX dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,5 +40,14 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-//    implementation ("org.bytedeco:javacv-platform:1.5.7")
+
+    val camerax_version = "1.1.0-beta01"
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation("androidx.camera:camera-video:${camerax_version}")
+
+    implementation("androidx.camera:camera-view:${camerax_version}")
+    implementation("androidx.camera:camera-extensions:${camerax_version}")
+
 }
