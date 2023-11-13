6e8f13b244b99aa1e6721ba9edd0baee0b768c04
Alexis Lee <aclee2001@gmail.com>
2023-11-12 16:10:01 -0800

    Adding File Utility class for Image Stitching

---
+++ app/src/main/java/com/example/worldsticher360/FileUtil.java
@@ -0,0 +1,13 @@
+package com.example.worldsticher360;
+
+
+import android.content.Context;
+
+public class FileUtil {
+    private Context context;
+
+    public FileUtil(Context context){
+        this.context = context;
+
+    }
+}
