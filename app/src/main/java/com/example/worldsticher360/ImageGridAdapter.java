b465d1e95b1e1afd9c077e8181062cb0361266aa
Alexis Lee <aclee2001@gmail.com>
2023-11-17 14:17:59 -0800

    changes from the restart

--- app/src/main/java/com/example/worldsticher360/ImageGridAdapter.java
+++ app/src/main/java/com/example/worldsticher360/ImageGridAdapter.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import androidx.recyclerview.widget.RecyclerView;
-import com.bumptech.glide.Glide;
+//import com.bumptech.glide.Glide;
 
 import java.io.IOException;
 
